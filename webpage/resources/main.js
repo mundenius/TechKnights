c5ad52e3ca4c7b8bbb86eb12a698828eb9761c2d
mundenius <121872746+mundenius@users.noreply.github.com>
2023-07-10 19:02:11 -0400

    avances index vale

---
+++ webpage/resources/main.js
@@ -0,0 +1,8 @@
+// Funciones para insertar los archivos navbar y footer
+$(document).ready(function () {
+	$('#navbar').load('fijos/navbar.html');
+});
+
+$(document).ready(function () {
+	$('#footerfile').load('/footer.html');
+});
