64bea59f4e15f7e005853f68f4f9be67c6347a78
mundenius <121872746+mundenius@users.noreply.github.com>
2023-07-17 01:49:54 -0400

    arreglos finales responsividad de navbar, funciones js y arreglos de fuente

--- webpage/resources/app.js
+++ webpage/resources/app.js
@@ -5,12 +5,12 @@ const observer =  new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
         if(entry.isIntersecting){
-            entry.target.classList.add('show');
+            entry.target.classList.add('a-show');
         }else{
-            entry.target.classList.remove('show');
+            entry.target.classList.remove('a-show');
         }
     });
 });
 
-const hiddenElements = document.querySelectorAll('.hidden');
+const hiddenElements = document.querySelectorAll('.a-hidden');
 hiddenElements.forEach((el) => observer.observe(el));
--- webpage/resources/main.js
+++ webpage/resources/main.js
@@ -6,3 +6,27 @@ $(document).ready(function () {
 $(document).ready(function () {
 	$('#footerfile').load('/footer.html');
 });
+
+
+// Obtener los elementos de "Portafolio" y "Contacto"
+const portafolioItem = document.querySelector('.nav-item.portafolio');
+const contactoItem = document.querySelector('.nav-item.contacto');
+
+// Obtener el elemento del menú de "Servicios" y el dropdown
+const serviciosItem = document.querySelector('.nav-item.servicios');
+const serviciosDropdown = serviciosItem.querySelector('.dropdown-menu');
+
+// Agregar un evento al menú de "Servicios" para detectar su estado
+serviciosItem.addEventListener('click', function() {
+  // Verificar si el dropdown de "Servicios" está activo
+  const isActive = serviciosDropdown.classList.contains('show');
+
+  // Agregar o eliminar una clase a "Portafolio" y "Contacto" según el estado del dropdown
+  if (isActive) {
+    portafolioItem.classList.add('shift-down-p');
+    contactoItem.classList.add('shift-down-c');
+  } else {
+    portafolioItem.classList.remove('shift-down-p');
+    contactoItem.classList.remove('shift-down-c');
+  }
+});
